95d6b2b2c0d9ac28190ee515ed0e2f4609b1918e
John <johnriveragonzalez7@gmail.com>
2025-10-08 14:01:24 -0300

    fix: permitir mÃ©todo POST en ruta saml2/callback para recibir respuestas de Okta

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,8 @@ use JohnRiveraGonzalez\Saml2Okta\Controllers\Saml2Controller;
 Route::middleware('web')->group(function () {
     // Rutas SAML2
     Route::get('/saml2/login', [Saml2Controller::class, 'redirect'])->name('saml2.login');
-    Route::get('/saml2/callback', [Saml2Controller::class, 'callback'])->name('saml2.callback');
-    Route::get('/auth/callback', [Saml2Controller::class, 'callback'])->name('auth.callback');
+    Route::match(['get', 'post'], '/saml2/callback', [Saml2Controller::class, 'callback'])->name('saml2.callback');
+    Route::match(['get', 'post'], '/auth/callback', [Saml2Controller::class, 'callback'])->name('auth.callback');
     Route::get('/saml2/metadata', [Saml2Controller::class, 'metadata'])->name('saml2.metadata');
-Route::get('/admin/saml2-config', [Saml2Controller::class, 'config'])->name('saml2.config');
+    Route::get('/admin/saml2-config', [Saml2Controller::class, 'config'])->name('saml2.config');
 });
