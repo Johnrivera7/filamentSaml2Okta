3fda6e363606871a4bcd12c817ed24a81272b323
John <johnriveragonzalez7@gmail.com>
2025-10-08 15:27:41 -0300

    fix: simplificar normalizaciÃ³n de certificados

--- src/Pages/Saml2OktaSettingsPage.php
+++ src/Pages/Saml2OktaSettingsPage.php
@@ -359,16 +359,11 @@ class Saml2OktaSettingsPage extends SettingsPage
         $data['okta_id_field'] = $data['okta_id_field'] ?? 'okta_id';
         $data['field_mappings'] = $data['field_mappings'] ?? [];
         
-        // Normalizar certificados: eliminar \n literales y dejar solo saltos de línea reales
+        // Normalizar TODOS los certificados: eliminar \n literales y usar saltos reales
         $certificateFields = ['idp_x509_cert', 'sp_x509_cert', 'sp_private_key'];
         foreach ($certificateFields as $field) {
             if (isset($data[$field]) && !empty($data[$field])) {
-                // Eliminar todos los saltos de línea (tanto \n literales como reales)
-                $cleaned = str_replace(['\n', "\r\n", "\r", "\n"], '', $data[$field]);
-                // Eliminar espacios
-                $cleaned = str_replace(' ', '', $cleaned);
-                // Reconstruir con saltos de línea reales cada 64 caracteres
-                $data[$field] = $this->formatCertificate($cleaned);
+                $data[$field] = $this->formatCertificate($data[$field]);
             }
         }
         
